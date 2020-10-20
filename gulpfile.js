d68cbee98dd77d2fd291eb49d30c4993239ecaf8
Kadir <kadiramin_info@yahoo.com>
2020-10-20 13:12:27 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const gulp = require('gulp'),
-     gutil = require('gulp-util');
+     gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log',function(){
-    gutil.log('sucess');
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', async ()=>{
+    gulp.src(coffeeSources)
+        .pipe(coffee({bare:true})
+            .on('error',gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
